bdada81e8b7d7db879e67f54353ce2452dd19805
Kirila Ruseva <kruseva@2-C-7.42heilbronn.de>
2025-06-22 12:09:19 +0200

    forb functions removed for norm

--- src/object_array/add_functs.c
+++ src/object_array/add_functs.c
@@ -6,7 +6,7 @@
 /*   By: kruseva <kruseva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 19:00:05 by kruseva           #+#    #+#             */
-/*   Updated: 2025/06/19 19:12:57 by kruseva          ###   ########.fr       */
+/*   Updated: 2025/06/22 11:54:12 by kruseva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	vector_init(t_vector *vec, size_t element_size, size_t initial_capacity)
 	vec->data = malloc(element_size * initial_capacity);
 	if (!vec->data)
 	{
-		fprintf(stderr, "Error: Failed to allocate memory for vector\n");
+		printf("Error: Failed to allocate memory for vector\n");
 		exit(EXIT_FAILURE);
 	}
 	vec->size = 0;
--- src/object_array/array.c
+++ src/object_array/array.c
@@ -6,7 +6,7 @@
 /*   By: kruseva <kruseva@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 14:20:23 by kruseva           #+#    #+#             */
-/*   Updated: 2025/06/19 18:54:06 by kruseva          ###   ########.fr       */
+/*   Updated: 2025/06/22 11:51:24 by kruseva          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,7 @@ void	vector_init(t_vector *vec, size_t element_size, size_t initial_capacity)
 {
 	if (!vec || element_size == 0 || initial_capacity == 0)
 	{
-		fprintf(stderr,
-			"Error: Invalid parameters for vector initialization\n");
+		printf("Error: Invalid parameters for vector initialization\n");
 		exit(EXIT_FAILURE);
 	}
 	vec->data = NULL;
@@ -37,7 +36,7 @@ void	vector_init(t_vector *vec, size_t element_size, size_t initial_capacity)
 	vec->data = malloc(element_size * initial_capacity);
 	if (!vec->data)
 	{
-		fprintf(stderr, "Error: Failed to allocate memory for vector\n");
+		printf("Error: Failed to allocate memory for vector\n");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -46,8 +45,7 @@ void	init_object_vector(t_object_vector *vec, int initial_capacity)
 {
 	if (!vec || initial_capacity <= 0)
 	{
-		fprintf(stderr,
-			"Error: Invalid parameters for object vector initialization\n");
+		printf("Error: Invalid parameters for object vector initialization\n");
 		exit(EXIT_FAILURE);
 	}
 	vector_init(&vec->planes_vec, sizeof(t_plane), initial_capacity);
@@ -61,18 +59,18 @@ void	vector_resize(t_vector *vec, size_t new_capacity)
 
 	if (!vec || new_capacity == 0)
 	{
-		fprintf(stderr, "Error: Invalid parameters for vector_resize\n");
+		printf("Error: Invalid parameters for vector_resize\n");
 		exit(EXIT_FAILURE);
 	}
 	new_data = malloc(vec->element_size * new_capacity);
 	if (!new_data)
 	{
-		fprintf(stderr, "Error: Failed to allocate memory for vector\n");
+		printf("Error: Failed to allocate memory for vector\n");
 		exit(EXIT_FAILURE);
 	}
 	if (vec->data)
 	{
-		memcpy(new_data, vec->data, vec->element_size * vec->size);
+		ft_memcpy(new_data, vec->data, vec->element_size * vec->size);
 		free(vec->data);
 	}
 	vec->data = new_data;
@@ -85,7 +83,7 @@ void	vector_push_back(t_vector *vec, const void *element)
 
 	if (!vec || !element)
 	{
-		fprintf(stderr, "Error: Invalid parameters for vector_push_back\n");
+		printf("Error: Invalid parameters for vector_push_back\n");
 		exit(EXIT_FAILURE);
 	}
 	if (vec->size >= vec->capacity)
@@ -93,6 +91,6 @@ void	vector_push_back(t_vector *vec, const void *element)
 		vector_resize(vec, vec->capacity * 2);
 	}
 	dest = (char *)vec->data + (vec->size * vec->element_size);
-	memcpy(dest, element, vec->element_size);
+	ft_memcpy(dest, element, vec->element_size);
 	vec->size++;
 }
